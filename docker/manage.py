108b584abf9b454ee00742720b4c5c0d7d27d262
Liu <liuly0322@mail.ustc.edu.cn>
2022-08-21 10:50:10 +0800

    feat: python 管理脚本支持输出错误信息

--- docker/manage.py
+++ docker/manage.py
@@ -10,7 +10,14 @@ f.close()
 try:
     from subprocess import run
     result = run(['/usr/local/bin/python', 'main.py'], capture_output=True, timeout=1)
-    print(result.stdout.decode('utf-8')[:-1])
+    stdout = result.stdout.decode('utf-8')
+    stderr = result.stderr.decode('utf-8')
+    if stdout:
+        print(stdout, end='')
+    elif stderr:
+        print('错误信息：' + stderr, end='')
+    else:
+        print('程序没有输出...')
 except Exception:
     print('运行超时！')
 
